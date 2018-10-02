5c448b72972d662eed45e4a11249c81fb460634f
hannahhellwig <39677202+hannahhellwig@users.noreply.github.com>
2018-10-02 13:27:56 +0200

    made paus and start buttons

--- code/src/index.js
+++ code/src/index.js
@@ -4,15 +4,58 @@ import "./index.css"
 
 class Clock extends React.Component {
 
+  state = {
+    date: new Date()
+  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {date: new Date()};
+  // }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  stopTime = () => {
+    clearInterval(this.timerID)
+  }
+
+  startTime = () =>{
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+        <div className="buttonContainer">
+          <button onClick={this.stopTime}>Pause</button>
+          <button onClick={this.startTime}>Start</button>
+        </div>
       </div>
     )
   }
 
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+)
